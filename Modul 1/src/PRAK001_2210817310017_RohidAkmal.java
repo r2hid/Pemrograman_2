323eb55971eb0eb9e6b84ca08983af2d96402f2c
r2hid <114940953+r2hid@users.noreply.github.com>
2023-10-22 19:39:57 +0800

    Delete Modul 1 directory

--- Modul 1/src/PRAK001_2210817310017_RohidAkmal.java
+++
@@ -1,7 +0,0 @@
-public class PRAK001_2210817310017_RohidAkmal {
-    public static void main(String[] args) {
-        System.out.println("Selamat Pagi, Rohid Akmal");
-        System.out.println("Selamat Siang, Rohid Akmal");
-        System.out.println("Selamat Malam, Rohid Akmal");
-    }
-}
